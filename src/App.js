83ca4b880e93613e17a8b742441301494b525be7
Diego Diaz <diego@macbook-pro.home>
2020-06-21 23:24:26 -0400

    Add react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { hot } from "react-hot-loader/root";
+import React from "react";
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <h1>Hey</h1>
+  </div>
+);
+
+export default hot(App);
